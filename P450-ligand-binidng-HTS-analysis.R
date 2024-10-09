29df8c4901e30e05a95c390a1b93f197b598e7dc
ekfry <59614067+ekfry@users.noreply.github.com>
2024-10-09 10:47:58 -0400

    Update P450-ligand-binidng-HTS-analysis.R

--- P450-ligand-binidng-HTS-analysis.R
+++ P450-ligand-binidng-HTS-analysis.R
@@ -5,7 +5,7 @@
 #  absorbance, prints absolute and difference spectra for each assay, and estimated dissociation constants and ∆Amax
 #  values for each assay based on the one-site, specific binding model.
 
-DOI: 10.1016/j.jbc.2024.107799
+# DOI: 10.1016/j.jbc.2024.107799
 
 
 
